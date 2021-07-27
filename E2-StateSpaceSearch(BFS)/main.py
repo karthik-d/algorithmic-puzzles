1a9f5fa8b96b62d66bd9cb2a01b65682fd1153c9
Karthik-D <karthikdesingu2000@gmail.com>
2021-07-27 14:22:00 +0530

    Improved data structures used

--- E2-StateSpaceSearch(BFS)/main.py
+++ E2-StateSpaceSearch(BFS)/main.py
@@ -1,58 +1,75 @@
 from Queue import *
 from StateFunctions import *
 
+
 line = "----------------------------"
+
+
+def deduce_path(state, parents):
+	
+	def deduce_path_rec(state, path_seq):
+		this_parent = parents[state]
+		# Reached the root node
+		if this_parent is None:
+			return path_seq
+		# Prepend node
+		path_seq = [this_parent] + path_seq[:]
+		return deduce_path_rec(this_parent, path_seq)
+
+	return deduce_path_rec(state, [])
+
 
 def BFS():
 
-    all_paths = Queue([[INITIAL_STATE]])
-    state_space = Queue([INITIAL_STATE])
-    goal_paths = list()
-    goal_states = list()
-    explored_states = list()
+	state_space = Queue([INITIAL_STATE])
+	goal_states = list()
+	explored_states = set()
+	parents = {INITIAL_STATE: None}
 
-    while not state_space.is_empty():
+	while not state_space.is_empty():
 		# Get next state and its path
-        state = state_space.dequeue()
-        path = all_paths.dequeue()
+		state = state_space.dequeue()
 		# Skip iteration if state already explored
-        if state in explored_states:
-            continue
-        explored_states.append(state)
-		# Check if state is a goal-state
-        if is_goal_state(state):
-            goal_states.append(state)
-            goal_paths.append(path)
+		if state in explored_states:
+			continue
+		explored_states.add(state)
+		# Check if the state is a goal-state
+		if is_goal_state(state):
+			goal_states.append(state)
 		# Find the next states
-        fringe = get_next_states(state)
-        state_space.enqueue(fringe)
-		# Add path to each fringe state
-        for n_state in fringe:
-            new_path = list(path[:])
-            new_path.append(n_state)
-            all_paths.enqueue([new_path])
+		fringe = get_next_states(state)
+		state_space.enqueue(fringe)
+		# Add parent of each fringe state, if not already added
+		for new_state in fringe:
+			if new_state not in parents:
+				parents[new_state] = state 
 
-    return goal_states, goal_paths, explored_states
+	return goal_states, explored_states, parents 
+
 
 if __name__ == '__main__':
-	goal_states, goal_paths, explored_states = BFS()
+
+	goal_states, explored_states, parents = BFS()
+	print(parents)
+
 	print("\nDISTINCT EXPLORED STATES COUNT: ", len(explored_states))
+
 	print("\nGOAL STATES COUNT: ", len(goal_states))
+
 	print("\nINITIAL STATE")
 	print(INITIAL_STATE)
+
 	print("\nGOAL STATES")
 	for state in goal_states:
 		print(state)
+
 	print("\nEXPLORED STATES")
 	for state in explored_states:
 		print(state)
-	for i in range(len(goal_paths)):
-		print("\nPATH", i+1)
-		print("To reach", goal_paths[i][-1])
-		print(line)
-		for vertex in goal_paths[i][:-1]:
-			print(vertex)
-		print(goal_paths[i][-1], '--> GOAL STATE')
-		print(line)
+
+	for state in goal_states:
+		print("\nPATH to reach", state)
+		print("\n".join(map(str, deduce_path(state, parents))))
+		print(state, '--> GOAL STATE')
 
             
