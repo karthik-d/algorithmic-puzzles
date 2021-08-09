0b4eeb3c60ea4952a3c45a8934e222f65ac2de70
Karthik-D <karthikdesingu2000@gmail.com>
2021-08-09 14:52:54 +0530

    Tracking depth

--- E3-StateSpaceSearch(DFS)/main.py
+++ E3-StateSpaceSearch(DFS)/main.py
@@ -19,6 +19,13 @@ def deduce_path(state, parents):
 	return deduce_path_rec(state, [])
 
 
+def make_path_string(path, depths):
+	string_path = list()
+	for state in path:
+		string_path.append("{state} (Depth: {depth})".format(state=state, depth=depths[state]))
+	return string_path
+
+
 def DFS():
 
 	state_space = Stack([INITIAL_STATE])
@@ -47,8 +54,41 @@ def DFS():
 	return goal_states, explored_states, parents 
 
 
+def DepthLimited(limit):
 
+	state_space = Stack([INITIAL_STATE])
+	depth_track = Stack([0])
+	goal_states = list()
+	explored_states = set()
+	parents = {INITIAL_STATE: None}
+	depths = {INITIAL_STATE: 0}
 
+	while not state_space.is_empty():
+		# Get next state and its path
+		state = state_space.pop()
+		curr_depth = depth_track.pop()
+		# Skip iteration if state already explored
+		if state in explored_states:
+			continue
+		explored_states.add(state)
+		# Check if the state is a goal-state
+		if is_goal_state(state):
+			goal_states.append(state)
+		# Find the next states
+		fringe = get_next_states(state)
+		state_space.push(fringe)
+		# Add parent and depth of each fringe state, if not already added
+		fringe_depths_list = list()
+		for new_state in fringe:
+			if new_state not in parents:
+				parents[new_state] = state 
+				depths[new_state] = curr_depth+1
+				fringe_depths_list.append(curr_depth+1)
+			else:
+				fringe_depths_list.append(depths[new_state])
+		depth_track.push(fringe_depths_list)
+
+	return goal_states, explored_states, parents, depths 
 
 
 if __name__ == '__main__':
@@ -75,5 +115,13 @@ if __name__ == '__main__':
 		print("\nPATH to reach", state)
 		print("\n".join(map(str, deduce_path(state, parents))))
 		print(state, '--> GOAL STATE')
+
+	
+	goal_states, explored_states, parents, depths = DepthLimited(10)
+	for state in goal_states:
+		print("\nPATH to reach", state)
+		print("\n".join(make_path_string(deduce_path(state, parents), depths)))
+		print(state, '--> GOAL STATE')
+		
 
             
