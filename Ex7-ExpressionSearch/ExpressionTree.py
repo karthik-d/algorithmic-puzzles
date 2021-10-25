61a7061936199abe955d17e12764610ea93ca4bb
Karthik-D <karthikdesingu2000@gmail.com>
2021-10-26 02:02:57 +0530

    Scripts tested

--- Ex7-ExpressionSearch/ExpressionTree.py
+++ Ex7-ExpressionSearch/ExpressionTree.py
@@ -3,7 +3,7 @@ class ExpressionNode:
 	operations = [ '+', '-', '*', '/' ]
 
 	def __init__(self, value, assert_operation=True):
-		self.is_operand = value not in operations
+		self.is_operand = value not in self.operations
 		if assert_operation:
 			assert self.is_operand == False
 		# Set node values
@@ -23,21 +23,58 @@ class ExpressionTree:
 	Edges represent an operation (+, -, *, /)
 	"""
 
-	def __init__(self, operand_space, bitmask, root):
+	def __init__(self, operand_space, root_node, bitmask=None, evaluation=None):
+		"""
+		For constructing trees from scratch, root_node is assumed to be a single node
+		Bitmask is computed. Expression result is computed
+		For constructing trees from two other trees, bitmask must be supplied through the merge
+		operation. Expression evaluation must be passed
+		"""
 		self.operand_space = operand_space
-		self.bitmask = bitmask
-		self.root = root
+		self.root = root_node
+		# Bitmask set
+		if bitmask is None:
+			self.bitmask = self.get_bitmask(self.root)
+		else:
+			self.bitmask = bitmask
+		# Evaluation result
+		if evaluation is None:
+			self.evaluation = self.root.value 
+		else:
+			self.evaluation = evaluation
+
+	def display(self):
+		display_queue = [self.root]
+		while display_queue:
+			node = display_queue.pop(0)
+			print(node.value)
+			if node.left_child is not None:
+				display_queue.append(node.left_child)
+			if node.right_child is not None:
+				display_queue.append(node.right_child)	
+
+	def get_bitmask(self, operand_node):
+		return [ operand_node.value==x for x in self.operand_space ]
 
 	@classmethod
-	def merge_bitmasks(self, bitmask_1, bitmask_2):
+	def merge_bitmasks(cls, bitmask_1, bitmask_2):
 		return [ x or y for x,y in zip(bitmask_1, bitmask_2) ]
 
-	@classmethod
-	def get_bitmask(self, operand):
-		return [ operand==x for x in self.operand_space ]
+	@classmethod 
+	def evaluate(cls, operation, lhs, rhs):
+		if operation == '+':
+			return lhs + rhs 
+		elif operation == '-':
+			return lhs - rhs 
+		elif operation == '*':
+			return lhs * rhs 
+		elif operation == '/':
+			return lhs // rhs
 
 	@classmethod
-	def is_merge_valid(cls, lhs, rhs):
+	def is_merge_valid(cls, operation, lhs, rhs):
+		if operation == '/' and lhs.evaluation % rhs.evaluation != 0:
+			return False
 		if lhs.operand_space != rhs.operand_space:
 			return False 
 		# NAND-operation
@@ -53,16 +90,38 @@ class ExpressionTree:
 		lhs, rhs are trees
 		"""
 		# Ensure the nodes can be merged
-		assert is_merge_valid(lhs, rhs) == True
+		assert cls.is_merge_valid(operation_node.value, lhs, rhs) == True
 		# Merge nodes
 		operation_node.left_child = lhs.root
 		operation_node.right_child = rhs.root
 		# Make new tree
 		new_tree = ExpressionTree(
 			operand_space=lhs.operand_space,
+			root_node=operation_node,
 			bitmask=cls.merge_bitmasks(lhs.bitmask, rhs.bitmask),
-			root=operation_node
+			evaluation=cls.evaluate(operation_node.value, lhs.evaluation, rhs.evaluation)
 		)
 		return new_tree
-		
-		
+
+"""		
+def run_test():
+	operands = [1, 2, 3, 5]
+	root_node = ExpressionNode(5, False)
+	tree_1 = ExpressionTree(
+		operands,
+		root_node
+	)
+	root_node = ExpressionNode(1, False)
+	tree_2 = ExpressionTree(
+		operands,
+		root_node
+	)
+	operation = ExpressionNode('+', True)
+	tree = ExpressionTree.merge_trees(operation, tree_1, tree_2)
+	tree.display() 
+	print(operands)
+	print(tree.bitmask)
+	print(tree.evaluation)
+
+run_test()
+"""
