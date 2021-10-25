b5c52e7bd44ac12defc7371ba389a2dbe545cd8e
Karthik-D <karthikdesingu2000@gmail.com>
2021-10-26 01:34:04 +0530

    Expression tree improved

--- Ex7-ExpressionSearch/ExpressionTree.py
+++ Ex7-ExpressionSearch/ExpressionTree.py
@@ -9,7 +9,11 @@ class ExpressionNode:
 		# Set node values
 		self.value = value
 		self.left_child = None 
-		self.right_child = None
+		self.right_child = None 
+
+	def get_bitmask(self, value, operand_space):
+		return [ operand==x for x in operand_space ]
+
 
 
 class ExpressionTree:
@@ -19,27 +23,46 @@ class ExpressionTree:
 	Edges represent an operation (+, -, *, /)
 	"""
 
-	def __init__(self, operand_space):
+	def __init__(self, operand_space, bitmask, root):
 		self.operand_space = operand_space
-		self.bitmask = [ False for _ in operand_space ]
-		self.root = None
+		self.bitmask = bitmask
+		self.root = root
 
+	@classmethod
+	def merge_bitmasks(self, bitmask_1, bitmask_2):
+		return [ x or y for x,y in zip(bitmask_1, bitmask_2) ]
+
+	@classmethod
 	def get_bitmask(self, operand):
 		return [ operand==x for x in self.operand_space ]
 
-	def check_merge_valid()
+	@classmethod
+	def is_merge_valid(cls, lhs, rhs):
+		if lhs.operand_space != rhs.operand_space:
+			return False 
+		# NAND-operation
+		# Same operand must NOT have been used in both already
+		for x_bit, y_bit in zip(lhs.bitmask, rhs.bitmask):
+			if x_bit and y_bit:
+				return False 
+		return True
 
 	@classmethod
 	def merge_trees(cls, operation_node, lhs, rhs):
 		""" 
 		lhs, rhs are trees
 		"""
+		# Ensure the nodes can be merged
+		assert is_merge_valid(lhs, rhs) == True
 		# Merge nodes
 		operation_node.left_child = lhs.root
 		operation_node.right_child = rhs.root
 		# Make new tree
-		new_tree = ExpressionTree(self.operand_space)
-		new_tree.root = operation_node
+		new_tree = ExpressionTree(
+			operand_space=lhs.operand_space,
+			bitmask=cls.merge_bitmasks(lhs.bitmask, rhs.bitmask),
+			root=operation_node
+		)
 		return new_tree
 		
 		
